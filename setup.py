a997b512563302f054b4092f0e75ef5f13db9198
rafaelgreca <rgvieira97@gmail.com>
2022-08-29 19:20:28 -0300

    Initializing the repo with originals Voxseg files

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name="voxseg-pytorch",
+    version="0.1.0",
+    author="Rafael Greca",
+    author_email="rafaelgreca97@hotmail.com",
+    description="Voxseg VAD implemented in PyTorch",
+    long_description=readme,
+    url="https://github.com/rafaelgreca/voxseg-pytorch",
+    packages=find_packages(),
+    license=license,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'wheel',
+        'pandas', 
+        'scipy', 
+        'tables',
+        'python_speech_features',
+        'tensorflow',
+    ],
+)
